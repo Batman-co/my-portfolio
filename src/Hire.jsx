7a0a7985afa1994e8ca16f929ec9d5c598ed6990
Batman-co <vishwanathk807@gmail.com>
2025-01-24 23:17:03 +0530

    animations

--- src/Hire.jsx
+++ src/Hire.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import union from "./Images/Union.svg";
 import { Element, Link } from "react-scroll";
-
+import { delay, motion } from "framer-motion";
 
 function Hire() {
   return (
@@ -10,24 +10,58 @@ function Hire() {
       <text className="font-poppins font-semibold text-[100px] bg-clip-text bg-gradient-to-r  from-[#4287f5] to-[#C73C7F] mt-7 text-transparent">
         Hire me
       </text>
+
       <div className="absolute bottom-40 h-auto w-auto">
-        <div className="absolute right-28 top-12 text-center">
-          <h1 className="text-[35px] font-semibold">Email</h1>
-          <a
-            href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=CllgCJqSvdHJQLJVCxKBbfmnBjvmmnznSDPsVTjtxrJxrXJVmxQjHDHgLCRLkPbqFKLGvWCjdwL"
-            className="text-[25px]  font-light font-poppins text-[#323232] hover:tracking-widest transition-all duration-500"
+        <motion.div
+          initial="initial"
+          whileHover="hovered"
+          className="absolute right-28 h-auto w-auto overflow-hidden top-12 text-center"
+        >
+          <motion.div variants={{ initial: { y: 0 }, hovered: { y: -100 } }} transition={{ duration: 0.2 }}>
+            <h1 className="text-[35px] font-semibold">Email</h1>
+            <a
+              href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=CllgCJqSvdHJQLJVCxKBbfmnBjvmmnznSDPsVTjtxrJxrXJVmxQjHDHgLCRLkPbqFKLGvWCjdwL"
+              className="text-[25px]  font-light font-poppins text-[#323232] transition-all duration-500"
+            >
+              vishwanathk807@gmail.com
+            </a>
+          </motion.div>
+          <motion.div variants={{ initial: { y: 100 }, hovered: { y: -100 } }} transition={{ duration: 0.2 }} className="absolute ">
+            <h1 className="text-[35px] font-semibold">Email</h1>
+            <a
+              href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=CllgCJqSvdHJQLJVCxKBbfmnBjvmmnznSDPsVTjtxrJxrXJVmxQjHDHgLCRLkPbqFKLGvWCjdwL"
+              className="text-[25px]  font-light font-poppins text-[#323232] transition-all duration-500"
+            >
+              vishwanathk807@gmail.com
+            </a>
+          </motion.div>
+        </motion.div>
+
+        <motion.div
+          initial="initial"
+          whileHover="hovered"
+          className="absolute text-center h-auto w-auto left-48 bottom-20 overflow-hidden"
+        >
+          <motion.div
+            variants={{ initial: { y: 0 }, hovered: { y: -100 } }}
+            transition={{ duration: 0.2 }}
           >
-            vishwanathk807@gmail.com
-          </a>
-        </div>
-        <div className="absolute left-48 bottom-20 text-center">
-          <h1 className="text-[35px] font-semibold">Phone</h1>
-          <a
-            className="text-[25px]  font-light font-poppins text-[#323232] hover:tracking-widest transition-all duration-500"
+            <h1 className="text-[35px] font-semibold">Phone</h1>
+            <a className="text-[25px]  font-light font-poppins text-[#323232]   transition-all duration-500">
+              8946018858
+            </a>
+          </motion.div>
+          <motion.div
+            variants={{ initial: { y: 100 }, hovered: { y: -100 } }}
+            transition={{ duration: 0.2 }}
+            className="absolute"
           >
-            8946018858
-          </a>
-        </div>
+            <h1 className="text-[35px] font-semibold">Phone</h1>
+            <a className="text-[25px]  font-light font-poppins text-[#323232]  transition-all duration-500">
+              8946018858
+            </a>
+          </motion.div>
+        </motion.div>
 
         <img src={union} />
       </div>
